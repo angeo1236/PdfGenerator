780f27b3166d17ee8d389379b6f59068c56b068b
Akhil Alex <akhil.alex@abzer.com>
2020-01-03 17:58:32 +0530

    html content print

--- app/src/main/java/com/abzer/pdfgenerator/MainActivity.java
+++ app/src/main/java/com/abzer/pdfgenerator/MainActivity.java
@@ -1,7 +1,10 @@
 package com.abzer.pdfgenerator;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.print.PdfPrint;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 import android.print.PrintManager;
@@ -9,10 +12,16 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
+    String k="angeo";
     Button savebtn;
 
     @Override
@@ -22,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webview);
         savebtn = findViewById(R.id.savebtn);
-        String webData = "<!DOCTYPE html><head> <meta http-equiv=\"Content-Type\" " +
-                "content=\"text/html; charset=utf-8\"> <html><body>şlkasşldkasşdksaşdkaşskdşk" +
-                "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" +
+        String webData = "<!DOCTYPE html><meta http-equiv=\"Content-Type\" " +
+                "content=\"text/html; charset=utf-8\"><body>şlkasşldkasşdksaşdkaşskdşk" +
+                "<p>gjhgjkg</p>" +k+ "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" +
                 "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" +
                 "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" +
                 "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" + "<p>gjhgjkg</p>" +
@@ -34,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
                 "" +
                 "<ul><li>123</li><li>123</li></ul>" +
+                "<hr style='border-top: 2px dashed grey'>" +
                 "</body></html>";
 
 
@@ -42,12 +52,20 @@ public class MainActivity extends AppCompatActivity {
         savebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createWebPrintJob(webView);
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP){
+                    // Do something for lollipop and above versions
+                    createWebPrintJob(webView);
+
+                } else{
+                    // do something for phones running an SDK before lollipop
+                    createWebPrintJobinWeb(webView);
+                }
+
             }
         });
     }
 
-    private void createWebPrintJob(WebView webView) {
+    private void createWebPrintJobinWeb(WebView webView) {
 
         PrintManager printManager = (PrintManager) this
                 .getSystemService(Context.PRINT_SERVICE);
@@ -62,4 +80,18 @@ public class MainActivity extends AppCompatActivity {
                     new PrintAttributes.Builder().build());
         }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void createWebPrintJob(WebView webView) {
+        String jobName = getString(R.string.app_name) + " Document";
+        PrintAttributes attributes = new PrintAttributes.Builder()
+                .setMediaSize(PrintAttributes.MediaSize.ISO_A4)
+                .setResolution(new PrintAttributes.Resolution("pdf", "pdf", 600, 600))
+                .setMinMargins(PrintAttributes.Margins.NO_MARGINS).build();
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/PDFTest/");
+        PdfPrint pdfPrint = new PdfPrint(attributes);
+        pdfPrint.print(webView.createPrintDocumentAdapter(jobName), path, "output_" + System.currentTimeMillis() + ".pdf");
+    }
+
 }
+
